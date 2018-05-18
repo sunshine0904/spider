19b7261feecb2f2d14b7a5410f46483e7d877692
sunshine0904 <sunshineboylei@gmail.com>
2018-05-18 18:47:01 +0800

    merge:debug json parse

---
+++ json_parse/json_par.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import json
+
+'''
+def parse json():
+    f = open("1.json",encoding='utf-8')
+    setting = json.load(f)
+'''
+with open("1.json",'r') as load_f:
+    load_dict = json.load(load_f)
+    #print(load_dict)
+    
+    global content
+    global each_content
+
+    for key,value in load_dict.items():
+        if key == "date":
+            print key+":"+value
+        if key == "msgs":
+            print type(key),key,type(value)
+        
+        content = value
+       
+        print type(content)
+        for key in content:
+            print type(key)
+            if type(key)  != dict:
+               continue
+            for key,value in key.items():
+                #print type(key),otype(value)
+                print key,value
+        
+
+
